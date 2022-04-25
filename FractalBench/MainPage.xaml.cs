26842bd0b3cc5417eb2bc2f16da4681f7ed1e2bc
Konstan <55129891+1800Kon@users.noreply.github.com>
2022-04-25 18:18:18 +0200

    mutithreaded properly

--- FractalBench/MainPage.xaml.cs
+++ FractalBench/MainPage.xaml.cs
@@ -1,14 +1,13 @@
 ﻿using FractalBench.Classes;
-using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Threading.Tasks;
-using Windows.Foundation;
-using Windows.UI;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Threading;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Media.Imaging;
 using WinRTXamlToolkit.Controls.DataVisualization.Charting;
+using Image = System.Drawing.Image;
 
 namespace FractalBench
 {
@@ -17,164 +16,99 @@ namespace FractalBench
         public MainPage()
         {
             InitializeComponent();
+                      
         }
 
         private void Render_Click(object sender, RoutedEventArgs e)
         {
             CreateFractal(400, 400, 4);
+
         }
 
-        private void CreateFractal(int height, int width, int noOfThreads)
+        private void CreateFractal(int width, int height, int noOfThreads) {
+            var startingSectionX = 0;
+            var startingSectionY = 0;
+            var endingSectionX = 0;
+            var endingSectionY = 0;
+
+            var bitmap = new Bitmap(width, height);
+            var rect = new Rectangle(0, 0, width, height);
+            var bitmapData = bitmap.LockBits(rect, ImageLockMode.ReadWrite, bitmap.PixelFormat);
+            var depth = Image.GetPixelFormatSize(bitmapData.PixelFormat) / 8; // this is something which might need to be changed
+            var buffer = new byte[bitmapData.Width * bitmapData.Height * depth];
+            
+            // Copy all the data from the bitmap to the buffer
+            Marshal.Copy(bitmapData.Scan0, buffer, 0, buffer.Length);
+            
+            ThreadPool.SetMaxThreads(noOfThreads, noOfThreads);
+            ThreadPool.SetMinThreads(noOfThreads, noOfThreads);
+            
+            for (var i = 0; i <= 3; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        startingSectionX = 0;
+                        startingSectionY = 0;
+                        endingSectionX = 200;
+                        endingSectionY = 200;
+                        break;
+
+                    case 1:
+                        startingSectionX = 200;
+                        startingSectionY = 0;
+                        endingSectionX = 400;
+                        endingSectionY = 200;
+                        break;
+
+                    case 2:
+                        startingSectionX = 0;
+                        startingSectionY = 200;
+                        endingSectionX = 200;
+                        endingSectionY = 400;
+                        break;
+
+                    case 3:
+                        startingSectionX = 200;
+                        startingSectionY = 200;
+                        endingSectionX = 400;
+                        endingSectionY = 400;
+                        break;
+                }
+                ThreadPool.QueueUserWorkItem(a => CreateSingleSection(startingSectionX, startingSectionY, endingSectionX, endingSectionY, height, width, buffer, depth));
+            }
+            Marshal.Copy(buffer, 0, bitmapData.Scan0, buffer.Length);
+            bitmap.UnlockBits(bitmapData);
+            bitmap.Save(@"E:\fractal\fractal.jpg", ImageFormat.Jpeg);
+        }
+
+        private static void CreateSingleSection(int startingSectionX, int startingSectionY, int endingSectionX, int endingSectionY, int height, int width, byte[] buffer, int depth)
         {
-            List<WriteableBitmap> listOfBitmaps = new List<WriteableBitmap>();
-            List<ConcurrentQueue<int>> listData = new List<ConcurrentQueue<int>>();
-
-            ConcurrentQueue<int> listToUse = null;
-            WriteableBitmap finalResult = BitmapFactory.New(width, height);
-
-            int i = 0;
-
-            WriteableBitmap writeableBmp0 = BitmapFactory.New(width, height);
-            WriteableBitmap writeableBmp1 = BitmapFactory.New(width, height);
-            WriteableBitmap writeableBmp2 = BitmapFactory.New(width, height);
-            WriteableBitmap writeableBmp3 = BitmapFactory.New(width, height);
-
-            ConcurrentQueue<int> bitmapData0 = new ConcurrentQueue<int>();
-            ConcurrentQueue<int> bitmapData1 = new ConcurrentQueue<int>();
-            ConcurrentQueue<int> bitmapData2 = new ConcurrentQueue<int>();
-            ConcurrentQueue<int> bitmapData3 = new ConcurrentQueue<int>();
-
-            listOfBitmaps.Add(writeableBmp0);
-            listOfBitmaps.Add(writeableBmp1);
-            listOfBitmaps.Add(writeableBmp2);
-            listOfBitmaps.Add(writeableBmp3);
-
-            Parallel.For(i, 2, new ParallelOptions { MaxDegreeOfParallelism = noOfThreads, }, p =>
-           {
-               int startingSectionX = 0;
-               int startingSectionY = 0;
-               int endingSectionX = 0;
-               int endingSectionY = 0;
-               switch (i)
-               {
-                   case 0:
-                       startingSectionX = 0;
-                       startingSectionY = 0;
-                       endingSectionX = 200;
-                       endingSectionY = 200;
-                       listToUse = bitmapData0;
-                       break;
-
-                   case 1:
-                       startingSectionX = 200;
-                       startingSectionY = 0;
-                       endingSectionX = 400;
-                       endingSectionY = 200;
-                       listToUse = bitmapData1;
-                       break;
-
-                   case 2:
-                       startingSectionX = 0;
-                       startingSectionY = 200;
-                       endingSectionX = 200;
-                       endingSectionY = 400;
-                       listToUse = bitmapData2;
-                       break;
-
-                   case 3:
-                       startingSectionX = 200;
-                       startingSectionY = 200;
-                       endingSectionX = 400;
-                       endingSectionY = 400;
-                       listToUse = bitmapData3;
-                       break;
-               }
-               for (int x = startingSectionX; x < endingSectionX; x++)
-               {
-                   for (int y = startingSectionY; y < endingSectionY; y++)
-                   {
-                       double a = (double)(x - (width / 4)) / (double)(width / 4);
-                       double b = (double)(y - (height / 4)) / (double)(height / 4);
-                       ComplexNumber c = new ComplexNumber(a, b);
-                       ComplexNumber z = new ComplexNumber(0, 0);
-
-                       int iterations = 0;
-                       do
-                       {
-                           iterations++;
-                           z.Square();
-                           z.Add(c);
-                           if (z.Magnitude() > 2.0)
-                           {
-                               break;
-                           }
-                       } while (iterations < 10000);
-
-                       if (iterations < 10000)
-                       {
-                           listToUse.Enqueue(x);
-                           listToUse.Enqueue(y);
-                           listToUse.Enqueue(iterations < 10000 ? 1 : 2);
-                       }
-                   }
-               }
-               i++;
-           });
-
-            listData.Add(bitmapData0);
-            listData.Add(bitmapData1);
-            listData.Add(bitmapData2);
-            listData.Add(bitmapData3);
-
-            for (int q = 0; q < listOfBitmaps.Count; q++)
+            for (var x = startingSectionX; x < endingSectionX; x++)
             {
-                WriteableBitmap bitmap = listOfBitmaps[q];
-                int[] queueToArray = listData[q].ToArray();
-
-                // everything below is ok
-                for (int o = 0; o < queueToArray.Length; o += 3)
+                for (var y = startingSectionY; y < endingSectionY; y++)
                 {
-                    Color color;
-
-                    if (queueToArray[0 + 2].Equals(1))
+                    var a = (double)(x - width / 4) / (width / 4);
+                    var b = (double)(y - height / 4) / (height / 4);
+                    var c = new ComplexNumber(a, b);
+                    var z = new ComplexNumber(0, 0);
+                    var iterations = 0;
+                    
+                    do
                     {
-                        color = Colors.DeepSkyBlue;
-                    }
-                    else
-                    {
-                        color = Colors.DeepPink;
-                    }
-                    bitmap.SetPixel(queueToArray[o], queueToArray[o + 1], color);
-                }
-            }
-
-
-            // This does not have to be multithreaded
-            foreach (var bitmap in listOfBitmaps)
-            {
-                using (finalResult.GetBitmapContext())
-                {
-                    using (bitmap.GetBitmapContext())
-                    {
-                        switch (listOfBitmaps.IndexOf(bitmap))
+                        iterations++;
+                        z.Square();
+                        z.Add(c);
+                        if (z.Magnitude() > 2.0)
                         {
-                            case 0:
-                                finalResult.Blit(new Rect(0, 0, 200, 200), bitmap, new Rect(0, 0, 200, 200), WriteableBitmapExtensions.BlendMode.Additive);
-                                break;
-                            case 1:
-                                finalResult.Blit(new Rect(200, 0, 200, 200), bitmap, new Rect(200, 0, 200, 200), WriteableBitmapExtensions.BlendMode.Additive);
-                                break;
-                            case 2:
-                                finalResult.Blit(new Rect(0, 200, 200, 200), bitmap, new Rect(0, 200, 200, 200), WriteableBitmapExtensions.BlendMode.Additive);
-                                break;
-                            case 3:
-                                finalResult.Blit(new Rect(200, 200, 200, 200), bitmap, new Rect(200, 200, 200, 200), WriteableBitmapExtensions.BlendMode.Additive);
-                                break;
+                            break;
                         }
-                    }
+                    } while (iterations < 10000);
+                    var offset = ((y * width) + x) * depth;
+                    var o = 0.2126 * buffer[offset + 0] + 0.7152 * buffer[offset + 1] + 0.0722 * buffer[offset + 2];
+                    buffer[offset + 0] = buffer[offset + 1] = buffer[offset + 2] = (byte)o;
                 }
             }
-            fractalImage.Source = finalResult;
         }
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
@@ -190,7 +124,7 @@ namespace FractalBench
             lstSource.Add(new Chart() { Utilization = 35, Time = 3 });
             lstSource.Add(new Chart() { Utilization = 20, Time = 4 });
             lstSource.Add(new Chart() { Utilization = 15, Time = 5 });
-            (LineChart.Series[0] as ColumnSeries).ItemsSource = lstSource;
+            ((ColumnSeries)LineChart.Series[0]).ItemsSource = lstSource;
         }
     }
 }
