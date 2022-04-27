5fee8af9a0e9208cb0068f2a6e893b91c37d595d
tungxuando <72494652+tungxuando@users.noreply.github.com>
2022-04-27 14:11:23 +0200

    Revert "Update"
    This reverts commit 1f320351c0acc377e61a8c7addece46e188bc01b.

--- FractalBench/BlankPage1.xaml.cs
+++ FractalBench/BlankPage1.xaml.cs
@@ -38,6 +38,11 @@ namespace FractalBench
             TextBlock1.Text = PopulateCPUInfo().ToString();
 
             timing.Text = checkTime().ToString();
+            //var time = checkTime();
+            //for (int y = 0; y < time; y++)
+            //{
+            //    LoadChartContents(time);
+            //}
             LoadChartContents();
         }
 
