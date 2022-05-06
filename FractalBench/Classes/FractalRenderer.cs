8c6d1eed5a8e9a542387ae8984c831881a105fa9
Konstan <55129891+1800Kon@users.noreply.github.com>
2022-05-06 12:30:57 +0200

    Comments

--- FractalBench/Classes/FractalRenderer.cs
+++ FractalBench/Classes/FractalRenderer.cs
@@ -12,6 +12,7 @@ namespace FractalBench.Classes
     {
         public async Task<WriteableBitmap> CreateFractal(int width, int height, int noOfThreads)
         {
+            // The list of tasks in which needs to awaited
             var tasks = new List<Task>();
             var semaphore = new Semaphore(noOfThreads, noOfThreads);
             var startingSectionX = 0;
@@ -26,6 +27,7 @@ namespace FractalBench.Classes
 
             for (var i = 0; i <= 3; i++)
             {
+                // Switch to determine the starting section of the image from which to generate the fractal, each being 1/4 of the image
                 switch (i)
                 {
                     case 0:
@@ -61,14 +63,16 @@ namespace FractalBench.Classes
                 var y = startingSectionY;
                 var sectionX = endingSectionX;
                 var sectionY = endingSectionY;
+                // Add the tasks to the list
                 tasks.Add(Task.Run(() =>
                 {
+                    // Wait for the semaphore to be available
                     semaphore.WaitOne();
                     CreateSingleSection(x, y, sectionX, sectionY, height,
                         width, buffer, semaphore);
                 }));
             }
-
+            // Wait for all the tasks to finish
             var final = Task.WhenAll(tasks);
             try
             {
@@ -78,7 +82,7 @@ namespace FractalBench.Classes
             {
                 // ignored
             }
-
+            // Copy the buffer to the bitmap
             buffer.CopyTo(bitmap.PixelBuffer);
             return final.Status == TaskStatus.RanToCompletion ? bitmap : null;
         }
@@ -86,6 +90,7 @@ namespace FractalBench.Classes
         private static void CreateSingleSection(int startingSectionX, int startingSectionY, int endingSectionX,
             int endingSectionY, int height, int width, IList<byte> buffer, Semaphore semaphore)
         {
+            // Fractal algorithm
             for (var x = startingSectionX; x < endingSectionX; x++)
             {
                 for (var y = startingSectionY; y < endingSectionY; y++)
@@ -105,7 +110,6 @@ namespace FractalBench.Classes
                             break;
                         }
                     } while (iterations < 100000);
-
                     // Color the bitmap
                     if (iterations < 100000)
                     {
@@ -121,6 +125,7 @@ namespace FractalBench.Classes
                     }
                 }
             }
+            // Release the semaphore
             semaphore.Release();
         }
     }
